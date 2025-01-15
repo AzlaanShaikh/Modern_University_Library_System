05355a4ff68c401ef979634aa4036941f5045924
Azlaan Shaikh <115040817+AzlaanShaikh@users.noreply.github.com>
2025-01-15 17:45:55 +0530

    Added HomePage

---
+++ components/BookList.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+interface Props{
+  title:string;
+  books:Book[];
+  containerClassName?:string
+}
+
+const BookList = ({title, books, containerClassName}: Props) => {
+  return (
+    <section>
+        <h2 className='font-bebas-neue text-4xl text-light-100'>{title}</h2>
+    </section>
+  )
+}
+
+export default BookList
